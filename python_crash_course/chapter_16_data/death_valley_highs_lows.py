a2c36dab03bce6c99593cab33357da60401a779f
Vlakoosh <vlakoosh@gmail.com>
2022-08-18 17:55:28 +0200

    Update death_valley_highs_lows.py

--- python_crash_course/chapter_16_data/death_valley_highs_lows.py
+++ python_crash_course/chapter_16_data/death_valley_highs_lows.py
@@ -13,24 +13,27 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     
     for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
         try:
             high = int(row[4])
             low = int(row[5])
-            date = datetime.strptime(row[2], "%Y-%m-%d")
         except ValueError:
-            print("No data")
-        dates.append(date) 
-        lows.append(low)
-        highs.append(high)
+            print(f"No data for {current_date}")
+        else:
+            dates.append(current_date) 
+            lows.append(low)
+            highs.append(high)
+        
+
 
 #Graph settings
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red', alpha=0.5)
 ax.plot(dates, lows, c='blue', alpha=0.5)
-ax.fill_between(dates, highs, lows, facecolor='black', alpha=0.3)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.3)
 
-ax.set_title("Highest temperature of the day - 2018", fontsize = 16)
+ax.set_title("Highest temperature of the day - 2018\nDeath Valley - California", fontsize = 16)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
