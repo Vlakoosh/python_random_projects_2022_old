ca2823a14aed7f1fddb78cef3a7ca1321760786a
Vlakoosh <vlakoosh@gmail.com>
2022-08-20 17:31:01 +0200

    cool fractal graph

---
+++ misc/barnsley_fern_fractal.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def one(x,y):
+    return (0., 0.16*y)
+def two(x,y):
+    return (0.85*x + 0.04*y, -0.04*x + 0.85*y + 1.6)
+def three(x,y):
+    return (0.2*x - 0.26*y, 0.23*x + 0.22*y + 1.6)
+def four(x,y):
+    return (-0.15*x + 0.28*y, 0.26*x + 0.24*y + 0.44)
+functions = [one, two, three, four]
+
+points = 1000000
+x, y = 0, 0
+x_list = []
+y_list = []
+for i in range(points):
+    function = np.random.choice(functions, p=[0.01, 0.85, 0.07, 0.07])
+    x, y = function(x,y)
+    x_list.append(x)
+    y_list.append(y)
+
+
+plt.scatter(x_list, y_list, s=0.01, color='green')
+plt.show()
